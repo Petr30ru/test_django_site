ff1aae59a0354565127d6122c4ec063b0303d360
dm <ex.power.1994@gmail.com>
2024-12-05 20:08:27 +0300

    add boostrap

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,6 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    name_char = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    time = models.CharField(max_length=200)
-    value = models.TextField()
-    # created_date = models.DateTimeField(default=timezone.now)
-    # published_date = models.DateTimeField(blank=True, null=True)
-
-    # def publish(self):
-    #     self.published_date = timezone.now()
-    #     self.save()
-
-    # def __str__(self):
-    #     return self.title
+    name_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    x_coord = models.TextField()
+    y_coord = models.TextField()
--- webbi/settings.py
+++ webbi/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 
 from pathlib import Path
-import os 
-import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +75,23 @@ WSGI_APPLICATION = 'webbi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ozon',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
